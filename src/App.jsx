f43b6aa7bba2ff789262847b6ddee92b4a1b2595
Suhaas Teja <Suhaas@zestiot.io>
2022-11-29 01:00:04 +0530

    uniq notes logic

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import FretBoard from './Components/Fretboard';
 
+const magicScale = ["t", "t", "t", "h", "t", "t", "t", "h"]
 const allNotes = ['a', 'a#', 'b', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#'];
 
 
@@ -31,6 +32,7 @@ function App() {
   });
 
   const [input, setInput] = useState('');
+  const [chord, setChord] = useState("");
 
   const handleFretClick = (string, fret) => {
     setFretData(prev => {
@@ -39,8 +41,46 @@ function App() {
       }
       const val = prev[string][fret];
       prev[string][fret] = val ? "" : getNoteName(string, fret);
+      getChordName(prev);
       return {...prev};
     })
+  }
+
+  const getChordName = (fretData) => {
+    const strings = Object.keys(fretData);
+    let notes = Object.values(fretData).map(arr => arr.filter(el => el).slice(-1));
+
+    notes.forEach((note, i) => {
+      if(!note[0]){
+        notes[i][0] = strings[i].toLowerCase();
+      }
+    });
+
+    notes.pop();
+
+    notes = [...new Set(notes.flat())];
+
+    const noteNumbers = [];
+    const l = notes.length;
+
+    const magicScaleObj = {}
+
+    notes.forEach((n, i) => {
+      // magicScaleObj[n] = 
+    })
+
+    // notes.forEach((n, i) => {
+    //   const j = allNotes.indexOf(n);
+    //   if(j){
+    //     noteNumbers.push(j)
+    //   }
+    // })
+
+    const chord = notes.join(", ");
+
+    setChord(chord);
+
+    console.log("notes", notes,"strings", strings, "chord", "chord","noteNumbers", noteNumbers, "fretData", fretData);
   }
 
   const handleClear = () => {
@@ -70,6 +110,7 @@ function App() {
     <div className="App flex">
       <Navbar handleClear={handleClear} input={input} handleInputChange={handleInputChange} handleFormSubmit={handleFormSubmit} />
       <FretBoard fretData={fretData} handleFretClick = {handleFretClick} />
+      <p>notes = {chord}</p>
     </div>
   )
 }
